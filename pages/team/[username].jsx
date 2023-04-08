c0ca99df7d3a69de7550ae2d153d48251ff8738b
Subhadip <mnhacker2001@gmail.com>
2023-04-09 04:11:05 +0530

    nothing

--- pages/team/[username].jsx
+++ pages/team/[username].jsx
@@ -57,14 +57,14 @@ const Username = () => {
                 <Link
                   key={i}
                   className={`flex items-center justify-center ${
-                    router.asPath === `/team/${data.username}`
+                    router.asPath === `/team/${data?.username}`
                       ? `underline`
                       : ``
                   }`}
-                  href={`/team/${data.username}`}
+                  href={`/team/${data?.username}`}
                 >
                   <li className="h-[calc(1.5rem+1.5vw)] flex justify-center items-center ">
-                    <span> {data.fullname}</span>
+                    <span> {data?.fullname}</span>
                   </li>
                 </Link>
               );
@@ -73,9 +73,9 @@ const Username = () => {
         </div>
         <div className="w-[75%] mx-auto h-[90%]">
           <div className="h-[30%] relative">
-            {user.fullname}
+            {user?.fullname}
             <div className="block w-[calc(8vw+10rem)] h-[calc(8vw+10rem)] top-[calc(3.5rem-1.5vw)] left-14 bg-[#CECECE] absolute z-30">
-              {user.username}
+              {user?.username}
             </div>
           </div>
           <div className="h-[70%] bg-[#E7E7E7]">Abhay Shah</div>
